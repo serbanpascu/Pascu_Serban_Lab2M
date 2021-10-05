f6e39607a7b764dfec0c46ac0f165a3135ba76f6
ȘERBAN PASCU <serban.pascu@stud.ubbcluj.ro>
2021-10-05 20:12:36 +0300

    Add project files.

---
+++ Pascu_Serban_Lab1/Data/LibraryContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pascu_Serban_Lab1.Models;
+
+namespace Pascu_Serban_Lab1.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext():base()
+        { }
+        public LibraryContext(DbContextOptions<LibraryContext> options) :
+       base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Order>().ToTable("Order");
+            modelBuilder.Entity<Book>().ToTable("Book");
+        }
+    }
+}
---
+++ Pascu_Serban_Lab1/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pascu_Serban_Lab1.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
