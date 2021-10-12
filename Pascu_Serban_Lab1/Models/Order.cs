bcd354a0b5c47304897c97d2c4c6149013826373
ȘERBAN PASCU <serban.pascu@stud.ubbcluj.ro>
2021-10-12 20:28:01 +0300

    Pascu_Serban_Lab3

--- Pascu_Serban_Lab1/Models/Order.cs
+++ Pascu_Serban_Lab1/Models/Order.cs
@@ -10,7 +10,7 @@ namespace Pascu_Serban_Lab1.Models
         public int OrderID { get; set; }
         public int CustomerID { get; set; }
         public int BookID { get; set; }
-
+        public DateTime OrderDate { get; set; }
         public Customer Customer { get; set; }
         public Book Book { get; set; }
     }
